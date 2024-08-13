290fbed070ae437cd36f18955d0a2552b056bdcb
Romin Mangroliya <romm@hms-networks.de>
2024-08-13 10:06:23 +0200

    01

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,54 @@
 import { useState } from 'react'
 import './App.css';
 
-
 function App() {
-
-  var ws;
-  var E = function(id) { return document.getElementById(id); }
-
-  var log = E('log');
+  
+  const getID = (id) => document.getElementById(id);
+  const getClass = (className) => document.getElementsByClassName(className);
 
   const connect = () => {
-    if (ws) { ws.close(); return; }
-    ws = new WebSocket(url.value);
-    console.log(document.getElementById('connect').value);
+    var ws;
+
+    if (ws) { ws.close();  return;  }
+
+    ws = new WebSocket(getID('connect').value);
+    console.log(getID('connect').value);
+
     if (!ws) return;
-    ws.onopen = function() { log.innerHTML += 'CONNECTION OPENED<br/>';};
+
+    ws.onopen = function() { getID('log').innerHTML += 'CONNECTION OPENED<br/>'; }
+
+    ws.onmessage = function(ev) { getID('log').innerHTML = `RECEIVED: ${ev.data}<br/>` + getID('log').innerHTML; }
 
     ws.onerror = function(ev) {
-      log.innerHTML += 'ERROR: ' + ev.message + '<br/>';
+      getID('log').innerHTML += 'ERROR: ' + ev.message + '<br/>';
       console.error('WebSocket Error:', ev);
-    }
+    };
 
     ws.onclose = function(ev) {
-      log.innerHTML += 'CONNECTION CLOSED (Code: ' + ev.code + ')<br/>';
-      enable(false);
+      getID('log').innerHTML += 'CONNECTION CLOSED (Code: ' + ev.code + ')<br/>';
       ws = null;
       console.log('WebSocket Closed:', ev);
-    }
-    
+    };
+  };
+
+
+  const can_Stop = () => {
+    console.log('CAN 1 STOP CAlled');
+    if (!ws) return;
+    ws.send(getClass('canStop').value);
+    getID('log').innerHTML = `SENT: CAN 1 STOP  <br/><hr>` + getID('log').innerHTML ; 
   }
 
   return (
-      <div>
-        <h1>WebSocket Test Client</h1>
-        {/* <input id="url" type="text" placeholder="Type URL" value={"ws://169.254.186.231:80/websocket"} /> */}
-        <button id="connect"  value={"ws://169.254.186.231:80/websocket"} onClick={connect} >Connect</button>
-
-        <div>Event log:</div>
-        <div id="log"></div>
-      </div>
-  )
+    <div>
+      <h1>WebSocket Test Client</h1>
+      <button id="connect" className='button url' value="ws://169.254.186.231:80/websocket" onClick={connect}>Connect</button>
+      <button className='button canStop' value="CAN 1 STOP" onClick={can_Stop}>Connect</button>
+      <div>Event log:</div>
+      <div id="log">hello</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
